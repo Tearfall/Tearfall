d2e99e9f7a5389d3a9efc2fded63e149c18c9b33
Mavi Joar C. Crisostomo <crisostomo.mavijoar2003@gmail.com>
2025-05-01 19:55:08 +0800

    Remove initial design, Import tailwindcss

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,36 +1,11 @@
 <template>
-  <body>
-    <Nav />
-    <HeroSection />
-    <AboutSection />
-    <ProjectSection />
-    <SkillsSection />
-    <ContactSection />
-    <Footer />
-  </body>
+   <p class="text-3xl font-bold underline">
+    Hello world!
+  </p>
 </template>
 
 <script>
-import Nav from './components/Nav.vue';
-import HeroSection from './components/HeroSection.vue';
-import AboutSection from './components/AboutSection.vue';
-import ProjectSection from './components/ProjectSection.vue';
-import SkillsSection from './components/SkillsSection.vue';
-import ContactSection from './components/ContactSection.vue';
-import Footer from './components/Footer.vue';
 
-  export default {
-    name: "App",
-    components: {
-      Nav,
-      HeroSection,
-      AboutSection,
-      ProjectSection,
-      SkillsSection,
-      ContactSection,
-      Footer,
-    },
-  };
 </script>
 
 <style></style>
